2dbe110a173b559a14e7db717a8799be61028079
hansen1416 <badapplesweetie@gmail.com>
2022-04-10 18:32:41 +0800

    coda

--- api/app/mod_auth/forms.py
+++ api/app/mod_auth/forms.py
@@ -1,8 +1,8 @@
 # Import Form and RecaptchaField (optional)
-from flask_module import Form  # , RecaptchaField
+from flask_wtf import Form  # , RecaptchaField
 
 # Import Form elements such as TextField and BooleanField (optional)
-from wtforms import TextField, PasswordField  # BooleanField
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
 
 # Import Form validators
 from wtforms.validators import Required, Email
@@ -11,7 +11,7 @@ from wtforms.validators import Required, Email
 # Define the login form (WTForms)
 
 class LoginForm(Form):
-    email = TextField('Email Address', [Email(),
-                                        Required(message='Forgot your email address?')])
+    email = StringField('Email Address', [Email(),
+                                          Required(message='Forgot your email address?')])
     password = PasswordField('Password', [
         Required(message='Must provide a password. ;-)')])
